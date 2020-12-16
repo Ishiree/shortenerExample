363975d68cb262266d1efbad5418f15f0846c6b5
ishiree <bangiksir@gmail.com>
2020-12-16 16:15:48 +0700

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Routing\Route;
+
+Route::view('/', 'welcome');
+Route::post('/register', 'Auth\RegisterController@register');
+Route::post('/login', 'Auth\LoginController@login');
+Route::post('/logout', 'Auth\LoginController@logout');
+Route::post('/password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+Route::post('/password/reset', 'Auth\ResetPasswordController@reset');
+
+Route::apiResource('/url', 'UrlController');
+
+Route::get('u/{url}', 'UrlController@show');
+Route::any('{url}', function () {
+    return view('welcome');
+})->where('url', '.*');
+
+Route::post('password/reset/{token}', function () {
+    return '';
+})->name('password.reset');
